676b40c16c3859b909e485040a769fd6c6a32f16
sergiy <sidose4@gmail.com>
2025-02-14 17:28:47 +0200

    fix remarks and codestyle

--- src/main/java/lessons/hw2/IsParenthesisValid.java
+++ src/main/java/lessons/hw2/IsParenthesisValid.java
@@ -4,58 +4,68 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+ * Checks if parenthesis string is valid.
+ */
 public class IsParenthesisValid {
 
-    public static boolean isValid(String str) {
-
-        if (str.isEmpty()) {
-            throw new RuntimeException("String is empty");
-        }
-
-        if (str.length() > 10000) {
-            throw new RuntimeException("String is out of range");
-        }
-
-        Map<Character, Character> pairs = new HashMap<Character, Character>();
-        pairs.put('(', ')');
-        pairs.put('{', '}');
-        pairs.put('[', ']');
-
-        Stack<Character> deck = new Stack<>();
-        char[] chArr = str.toCharArray();
-
-        for(int i = 0; i < chArr.length; i++) {
-            if(pairs.containsKey(chArr[i])) {
-                deck.push(chArr[i]);
-                Character search = pairs.get(chArr[i]);
-                chArr[i] = '_';
-                for(int j = 0; j < chArr.length; j++) {
-                    if(search == chArr[j]) {
-                        chArr[j] = '_';
-                        deck.pop();
-                        break;
-                    }
-                }
-            }
-
-        }
-
-        for (char c : chArr) {
-            if (c != '_')
-                return false;
-        }
-
-        return deck.isEmpty();
-
+  /**
+   * Checks if a string of parenthesis is valid.
+   *
+   * @param str A string to check.
+   * @return true if correct, false - otherwise.
+   */
+  public static boolean isValid(String str) {
+    if (str.isEmpty()) {
+      throw new RuntimeException("String is empty");
     }
 
-    public static void main(String[] args) {
-       String str1 = "()[]{}}{"; // correct
-       String str2 = "}]){[("; // correct
-       String str3 = "(][}){{"; // incorrect
-
-       System.out.println(isValid(str1)); // true
-       System.out.println(isValid(str2)); // true
-       System.out.println(isValid(str3)); // false
+    if (str.length() > 10000) {
+      throw new RuntimeException("String is out of range");
     }
+
+    Map<Character, Character> pairs = new HashMap<Character, Character>();
+    pairs.put('(', ')');
+    pairs.put('{', '}');
+    pairs.put('[', ']');
+    Stack<Character> deck = new Stack<>();
+    char[] chars = str.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      if (pairs.containsKey(chars[i])) {
+        deck.push(chars[i]);
+        Character search = pairs.get(chars[i]);
+        chars[i] = '_';
+        for (int j = 0; j < chars.length; j++) {
+          if (search == chars[j]) {
+            chars[j] = '_';
+            deck.pop();
+            break;
+          }
+        }
+      }
+    }
+    for (char c : chars) {
+      if (c != '_') {
+        return false;
+      }
+
+    }
+    return deck.isEmpty();
+
+  }
+
+  /**
+   * Main method.
+   *
+   * @param args Arguments.
+   */
+  public static void main(String[] args) {
+    String str1 = "()[]{}"; // correct
+    String str2 = "{[]}()"; // correct
+    String str3 = "([]){{"; // incorrect
+
+    System.out.println(isValid(str1)); // true
+    System.out.println(isValid(str2)); // true
+    System.out.println(isValid(str3)); // false
+  }
 }
--- src/main/java/lessons/hw2/ListNode.java
+++ src/main/java/lessons/hw2/ListNode.java
@@ -1,60 +1,80 @@
 package lessons.hw2;
 
+/**
+ * Class to operate with list nodes.
+ */
 public class ListNode {
-    int val;
-    ListNode next = null;
+  int val;
+  ListNode next = null;
 
-    ListNode() {}
-    ListNode(int val) {
-        this.val = val;
-    }
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
+  ListNode(int val) {
+    this.val = val;
+  }
 
-    public static ListNode reverseList(ListNode head) {
-        if(head == null) {
-            return null;
-        }
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 
-        ListNode current = new ListNode(head.val);
-
-        if(head.next == null) {
-            return current;
-        }
-
-        ListNode reverse = reverseList(head.next);
-
-        if (reverse.next == null) {
-            reverse.next = current;
-            return reverse;
-        }
-
-        ListNode last = reverse.next;
-        while (last.next != null)
-            last = last.next;
-
-        last.next = current;
-
-        return reverse;
+  /**
+   * Reverses list node.
+   *
+   * @param head Head of list node.
+   * @return Reversed list node.
+   */
+  public static ListNode reverseList(ListNode head) {
+    if (head == null) {
+      return null;
     }
 
-    public static void print(ListNode head) {
-        System.out.println(head.val);
-        if(head.next != null) {
-            print(head.next);
-        }
+    ListNode current = new ListNode(head.val);
+
+    if (head.next == null) {
+      return current;
     }
 
-    public static void main(String[] args) {
+    ListNode reverse = reverseList(head.next);
 
-        ListNode item_1 = new ListNode(1);
-        ListNode item_2 = new ListNode(2,item_1);
-        ListNode item_3 = new ListNode(3,item_2);
-        ListNode item_4 = new ListNode(4,item_3);
-        ListNode item_5 = new ListNode(5,item_4);
-
-        print(reverseList(item_5));
+    if (reverse.next == null) {
+      reverse.next = current;
+      return reverse;
     }
+
+    ListNode last = reverse.next;
+    while (last.next != null) {
+      last = last.next;
+    }
+
+    last.next = current;
+
+    return reverse;
+  }
+
+  /**
+   * Prints node list.
+   *
+   * @param head Head of list node.
+   */
+  public static void print(ListNode head) {
+    System.out.println(head.val);
+    if (head.next != null) {
+      print(head.next);
+    }
+  }
+
+  /**
+   * Main method.
+   *
+   * @param args Arguments.
+   */
+  public static void main(String[] args) {
+
+    ListNode item1 = new ListNode(1);
+    ListNode item2 = new ListNode(2, item1);
+    ListNode item3 = new ListNode(3, item2);
+    ListNode item4 = new ListNode(4, item3);
+    ListNode item5 = new ListNode(5, item4);
+
+    print(reverseList(item5));
+  }
 }
